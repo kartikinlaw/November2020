afb11152c3b6ad131d138eefbf745efdb7ddc124
thebtechviral <thebtechviral@gmail.com>
2020-11-15 21:10:48 +0530

    Files added for 15 Nov Session

--- 2 Say Hello World [8 Nov]/practice.cpp
+++ 2 Say Hello World [8 Nov]/practice.cpp
@@ -1,29 +1,19 @@
 #include<iostream>
 using namespace std;
 
-
 int main(){
-	//cout<<"Hello World by August Batch Mentees , who are awesome"<<endl;
-	// int num ;
-	// cout<<num<<endl;
-	// char ch ;
-	// cout<<ch<<endl;
-	// ch = 'a';
-	// num = 10;
-	// cout<<ch<<endl;
-	// cout<<num<<endl;
-	// string zuck = "I made Facebook";
-	// cout<<zuck<<endl;
-	int num1,num2,sum;
-	cout<<"Enter num1\n";
-	cin>>num1;
-	cout<<"Enter num2\n";
-	cin>>num2;
-	sum = num1+num2;
-	cout<<"The sum is "<<sum<<"\n";
+	cout<<"Hello World by November Batch Mentees , who are awesome"<<endl;
+	int var = 12;
+	string pokemon = "pikachu";
+	cout<<pokemon<<endl;
+/*
+This prints some string .ie 
+.Hello World by November Batch Mentees
+ , who are awesome .
+*/
 
-
-//This prints some string .ie .Hello World by August Batch Mentees , who are awesome .
+//Captain is looking very cool today.
+//You make great lies .
 	return 0;
 }
 
