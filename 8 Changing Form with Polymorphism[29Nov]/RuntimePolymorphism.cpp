936435c6f55887279ddb7d0900e44cfe58096228
thebtechviral <thebtechviral@gmail.com>
2020-11-29 21:12:01 +0530

    files added for 29 Nov 2020 Polymorphism

---
+++ 8 Changing Form with Polymorphism[29Nov]/RuntimePolymorphism.cpp
@@ -0,0 +1,34 @@
+#include <iostream>    
+using namespace std;    
+
+class Animal {    
+    public:    
+void eat()
+   {  
+	cout<<"Eating...";      
+   }        
+};    
+
+class Dog: public Animal      
+{     
+public:    
+ void eat()      
+    {           
+    	cout<<"Eating bread...";      
+    }  
+};  
+
+int main(void) { 
+
+// int num = 5;
+   Dog d = Dog(); 
+
+   // Dog()  ---> Creating an object of Dog class
+
+   // Dog d  ----> storing object created of Dog class in d .
+
+
+
+   d.eat();    
+   return 0;    
+}    
